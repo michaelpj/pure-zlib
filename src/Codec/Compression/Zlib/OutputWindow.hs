cdfa0b23a8edbfdec0af6f3bc4e0ce2957eb29ba
Michael Peyton Jones <me@michaelpj.com>
2021-10-19 17:09:43 +0100

    Switch to storable vectors

--- src/Codec/Compression/Zlib/OutputWindow.hs
+++ src/Codec/Compression/Zlib/OutputWindow.hs
@@ -18,11 +18,9 @@ module Codec.Compression.Zlib.OutputWindow (
 import Control.Monad (foldM)
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
-import qualified Data.ByteString.Short as SBS
-import Data.ByteString.Short.Internal (ShortByteString (SBS))
-import qualified Data.Primitive as Prim
-import qualified Data.Vector.Primitive as V
-import qualified Data.Vector.Primitive.Mutable as MV
+import qualified Data.Vector.Storable as V
+import qualified Data.Vector.Storable.Mutable as MV
+import Data.Vector.Storable.ByteString
 import GHC.ST (ST (..))
 import GHC.Word (Word8 (..))
 
@@ -51,13 +49,13 @@ emitExcess OutputWindow{owWindow = window, owNext = initialOffset}
     -- Need move as these can overlap!
     MV.move (MV.slice 0 excessLength window) (MV.slice excessChunkSize excessLength window)
     let ow' = OutputWindow window excessLength
-    return (Just (SBS.fromShort $ toByteString toEmit, ow'))
+    return (Just (toByteString toEmit, ow'))
 
 finalizeWindow :: OutputWindow s -> ST s S.ByteString
 finalizeWindow ow = do
   -- safe as we're doing it at the end
   res <- V.unsafeFreeze (MV.slice 0 (owNext ow) (owWindow ow))
-  pure $ SBS.fromShort $ toByteString res
+  pure $ toByteString res
 
 -- -----------------------------------------------------------------------------
 
@@ -73,7 +71,7 @@ addChunk !ow !bs = foldM copyChunk ow (L.toChunks bs)
 copyChunk :: OutputWindow s -> S.ByteString -> ST s (OutputWindow s)
 copyChunk ow sbstr = do
   -- safe as we're never going to look at this again
-  ba <- V.unsafeThaw $ fromByteString $ SBS.toShort sbstr
+  ba <- V.unsafeThaw $ fromByteString sbstr
   let offset = owNext ow
       len = MV.length ba
   MV.copy (MV.slice offset len (owWindow ow)) ba
@@ -86,7 +84,7 @@ addOldChunk (OutputWindow window next) dist len = do
   -- place. 'copyChunked' handles this for us.
   copyChunked (MV.slice next len window) (MV.slice (next - dist) len window) dist
   result <- V.freeze $ MV.slice next len window
-  return (OutputWindow window (next + len), SBS.fromShort $ toByteString result)
+  return (OutputWindow window (next + len), toByteString result)
 
 {- | A copy function that copies the buffers sequentially in chunks no larger than
  the stated size. This allows us to handle the insane zlib behaviour.
@@ -100,15 +98,8 @@ copyChunked dest src chunkSize = go 0 (MV.length src)
     MV.copy (MV.slice copied thisChunkSize dest) (MV.slice copied thisChunkSize src)
     go (copied + thisChunkSize) (toCopy - thisChunkSize)
 
--- TODO: these are a bit questionable. Maybe we can just pass around Vector Word8 in the client code?
-fromByteString :: SBS.ShortByteString -> V.Vector Word8
-fromByteString (SBS ba) =
-  let len = Prim.sizeofByteArray (Prim.ByteArray ba)
-      sz = Prim.sizeOf (undefined :: Word8)
-   in V.Vector 0 (len * sz) (Prim.ByteArray ba)
+fromByteString :: S.ByteString -> V.Vector Word8
+fromByteString = byteStringToVector
 
-toByteString :: V.Vector Word8 -> SBS.ShortByteString
-toByteString (V.Vector offset len ba) =
-  let sz = Prim.sizeOf (undefined :: Word8)
-      !(Prim.ByteArray ba') = Prim.cloneByteArray ba (offset * sz) (len * sz)
-   in SBS ba'
+toByteString :: V.Vector Word8 -> S.ByteString
+toByteString = vectorToByteString
